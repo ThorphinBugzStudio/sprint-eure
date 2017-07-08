84eeba442eed7cf39108d7cf5775ee066fdee0ef
Maximus <maximefauvelliere@gmail.com>
2017-07-08 11:45:38 +0200

    form inscription en cours

--- app/Views/users/inscription.php
+++ app/Views/users/inscription.php
@@ -4,7 +4,7 @@
 
 <h1>Section Inscription</h1>
 
-<form class="form-subscribe" action="" enctype="multipart/form-data" method="POST">
+<form style="display:flex;flex-direction:column;width:50%;" class="form-subscribe" action="" enctype="multipart/form-data" method="POST">
 
   <label for="firstname">Votre prénom</label>
   <input type="text" name="firstname" value="">
@@ -21,7 +21,13 @@
   <label for="avatar">Votre avatar</label>
   <input type="file" name="avatar" value="">
 
-  <div class="first-adress">
+  <label for="password">Votre mot de passe</label>
+  <input type="text" name="password" value="">
+
+  <label for="password-confirm">Confirmez votre mot de passe</label>
+  <input type="text" name="password-confirm" value="">
+
+  <div style="display:flex;flex-direction:column;" class="first-adress">
     <label for="adress_1">Adresse</label>
     <input type="text" name="adress_1" value="">
 
@@ -34,11 +40,53 @@
     <label for="country_1">Pays</label>
     <input type="text" name="country" value="">
 
-    <label for="adress_type">Adresse de</label>
+<?php $adresses_types = ['facturation'=> 'Adresse de facturation',
+                         'livraison' => 'Adresse de livraison']; ?>
+
+    <label for="adress_type_1">Type d'adresse</label>
 
     <select class="" name="">
+      <option value="">choisissez</option>
+      <?php foreach($adresses_types as $adresse => $value)
+      { ?>
+        <option value="<?= $adresse?>"<?php if(!empty($_POST['adresse'])){ if($_POST['adresse'] == $adresse){ echo 'selected="selected"';} } ?>><?= $value ?></option>
 
+    <?php  } ?>
     </select>
+
+  </div>
+
+  <input type="checkbox" name="check-facturation" value="">
+  <label style="font-size:0.7em" for="both">Si votre adresse de facturation est la même que votre adresse de livraison cochez cette case</label>
+
+
+  <div id=adresse_2 style="display:flex;flex-direction:column;" class="second-adress">
+    <label for="adress_2">Adresse</label>
+    <input type="text" name="adress_1" value="">
+
+    <label for="postal-code_2">Code postal</label>
+    <input type="text" name="postal-code_1" value="">
+
+    <label for="city_2">Ville</label>
+    <input type="text" name="city_1" value="">
+
+    <label for="country_2">Pays</label>
+    <input type="text" name="country" value="">
+
+<?php $adresses_types = ['facturation'=> 'Adresse de facturation',
+                         'livraison' => 'Adresse de livraison']; ?>
+
+    <label for="adress_type_2">Type d'adresse</label>
+
+    <select class="" name="">
+      <option value="">choisissez</option>
+      <?php foreach($adresses_types as $adresse => $value)
+      { ?>
+        <option value="<?= $adresse?>"<?php if(!empty($_POST['adresse'])){ if($_POST['adresse'] == $adresse){ echo 'selected="selected"';} } ?>><?= $value ?></option>
+
+    <?php  } ?>
+    </select>
+
   </div>
 
   <div class="">
@@ -46,13 +94,8 @@
   </div>
 
 
-  <label for="adress_2">Adresse 2</label>
 
-  <label for="password">Votre mot de passe</label>
-  <input type="text" name="password" value="">
 
-  <label for="password-confirm">Confirmez votre mot de passe</label>
-  <input type="text" name="password-confirm" value="">
 
 
 
