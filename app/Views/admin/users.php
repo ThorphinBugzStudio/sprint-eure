4acf58db295b091065b6ae8afd722724152108ba
herve <hpalluet@live.fr>
2017-07-09 20:33:59 +0200

    update users suite a travail en meme temps sur le fichier

--- app/Views/admin/users.php
+++ app/Views/admin/users.php
@@ -12,45 +12,43 @@
 
     <div class="panel panel-default" style="overflow-x:auto;">
       <table class="table table-striped table-bordered table-hover">
-         <!-- En-tête de tableau -->
-         <thead>
-            <tr>
-               <th>ID</th>
-               <th>Pseudonyme</th>
-               <th>Rôle</th>
-               <th>Status</th>
-               <th>Créé le</th>
-               <th>Dernière modification le</th>
-               <th>Action</th>
-            </tr>
-         </thead>
-         <tbody>
+        <!-- En-tête de tableau -->
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Pseudonyme</th>
+            <th>Rôle</th>
+            <th>Status</th>
+            <th>Créé le</th>
+            <th>Dernière modification le</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
           <?php $i = 0;
           foreach ($results as $result) { ?>
-           <tr>
-            <!--// Affichage si statut != deleted-->
-            <?php if ($result['status'] != 'deleted') { ?>
-              <td><?php echo $result['id']; ?></td>
-              <td><?php echo $result['username']; ?></td>
-              <td><?php echo $result['role']; ?></td>
-              <td><?php echo $result['status']; ?></td>
-              <td><?php echo $result['created_at']; ?></td>
-              <td><?php echo $result['modified_at']; ?></td>
-              <td class="menu_actions">
-                <!-- BOUTON : Edition -->
-                <a class="" href="<?=$this->url('admin_single_user', ['id' => $result['id']] ) ?>">
-                  <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Editer">
-                    <i class="fa fa-pencil" aria-hidden="true"></i>
-                  </button></a>
-                <!-- BOUTON : Delete si status != deleted -->
-                <a class="" href="<?=$this->url('admin_delete_user', ['id' => $result['id'], 'fromPage' => $actualPageId] ) ?>">
-                  <button type="button" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Supprimer">
-                    <i class="fa fa-trash-o" aria-hidden="true"></i>
-                  </button>
-                </a>
-              </td>  <?php } ?>
-          </tr>
-         <?php } ?>
+          <tr>
+            <td><?php echo $result['id']; ?></td>
+            <td><?php echo $result['username']; ?></td>
+            <td><?php echo $result['role']; ?></td>
+            <td><?php echo $result['status']; ?></td>
+            <td><?php echo $result['created_at']; ?></td>
+            <td><?php echo $result['modified_at']; ?></td>
+            <td class="menu_actions">
+              <!-- BOUTON : Edition -->
+              <a class="" href="<?=$this->url('admin_single_user', ['id' => $result['id']] ) ?>">
+                <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Editer">
+                  <i class="fa fa-pencil" aria-hidden="true"></i>
+                </button>
+              </a>
+              <!-- BOUTON : Delete -->
+              <a class="" href="<?=$this->url('admin_delete_user', ['id' => $result['id'], 'fromPage' => $actualPageId] ) ?>">
+                <button type="button" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Supprimer">
+                  <i class="fa fa-trash-o" aria-hidden="true"></i>
+                </button>
+              </a>
+            </td>
+          </tr> <?php } ?>
         </tbody>
       </table>
     </div>
@@ -63,4 +61,4 @@
   </div>
 </div>
 
-<?php $this->stop('main_content') ?>
+<?php $this->stop('main_content'); ?>
