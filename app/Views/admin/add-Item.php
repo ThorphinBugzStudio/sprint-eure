380d52638fff5281f32ef2b81a0dfc9606417916
Vox <DarkBlueMax@hotmail.fr>
2017-07-10 16:56:58 +0200

    Edit CSS Back + Front

--- app/Views/admin/add-Item.php
+++ app/Views/admin/add-Item.php
@@ -14,16 +14,16 @@
           <!-- Catégorie : Désignation -->
           <div class="designation">
             <label for="designation">Désignation : </label>
+            <input class="input-back" type="text" name="designation" id="designation" placeholder="Saisir un titre" value="<?php if(!empty($_POST['designation'])) { echo $_POST['designation'];} ?>" />
             <span class="error"><?php if(!empty($error['exist'])) { echo $error['exist']; } ?></span>
             <span class="error"><?php if(!empty($error['designation'])) { echo $error['designation']; } ?></span><br>
-            <input class="input-back" type="text" name="designation" id="designation" placeholder="Saisir un titre" value="<?php if(!empty($_POST['designation'])) { echo $_POST['designation'];} ?>" />
           </div>
 
           <!-- Catégorie : Description -->
           <div class="description mt-3">
             <label for="description">Description : </label>
-            <span class="error"><?php if(!empty($error['description'])) { echo $error['description']; } ?></span><br>
             <textarea name="description" rows="8" cols="80" class="input-back" placeholder="Saisir une description de l'article..."><?php if(!empty($_POST['description'])) { echo $_POST['description'];} else { if(!empty($article['description'])) { echo $article['description']; }} ?></textarea>
+            <span class="error"><?php if(!empty($error['description'])) { echo $error['description']; } ?></span><br>
           </div>
 
           <!-- Catégorie : Catégorie de produit -->
@@ -40,15 +40,15 @@
               <!-- Catégorie : Quantité -->
               <div class="col-lg-6 mr-auto quantite mt-3">
                 <label for="quantite">Quantité par packaging : </label>
-                <span class="error"><?php if(!empty($error['quantite'])) { echo $error['quantite']; } ?></span><br>
                 <input class="input-back" type="number" name="quantite" id="quantite" value="<?php if(!empty($_POST['quantite'])) { echo $_POST['quantite'];} ?>" />
+                <span class="error"><?php if(!empty($error['quantite'])) { echo $error['quantite']; } ?></span><br>
               </div>
 
               <!-- Catégorie : Prix HT -->
               <div class="col-lg-6 ml-auto prix mt-3">
                 <label for="prix">Prix HT : </label>
-                <span class="error"><?php if(!empty($error['prix'])) { echo $error['prix']; } ?></span><br>
                 <input class="input-back" type="text" name="prix" id="prix" value="<?php if(!empty($_POST['prix'])) { echo $_POST['prix'];} ?>" />
+                <span class="error"><?php if(!empty($error['prix'])) { echo $error['prix']; } ?></span><br>
               </div>
             </div>
         </div>
@@ -63,7 +63,7 @@
       <!-- Catégorie : Image à la une -->
         <div class="image m-4">
           <input type="file" name="image" style="max-width: 100%;">
-          <span class="error"><?php if(!empty($error['image'])) { echo $error['image']; } ?></span><br>
+          <?php if(!empty($error['image'])) { echo '<p class="error">' . $error['image'] . '</p>'; } ?>
         </div>
       </div>
 
@@ -72,9 +72,9 @@
           <i class="fa fa-gear fa-fw"></i> Paramètres
         </div>
         <!-- Catégorie : Article en avant ou non -->
-        <div class="checkbox mx-5 p-2">
+        <div class="checkbox mx-5 p-2 my-3">
           <INPUT type="checkbox" name="home" value="1"> Mettre l'article en avant sur la page d'accueil
-          <span class="error"><?php if(!empty($error['home'])) { echo $error['home']; } ?></span><br>
+          <?php if(!empty($error['home'])) { echo '<p class="error">' . $error['home'] . '</p>'; } ?>
         </div>
 
         <!-- Bouton d'envoie -->
