07f9ccf7f798b72be9f6231e06748debfe74ac31
herve <hpalluet@live.fr>
2017-07-11 19:26:03 +0200

    Back comments terminé

--- app/Views/admin/comments.php
+++ app/Views/admin/comments.php
@@ -45,8 +45,8 @@
               <!-- or BOUTON : Not Approve -->
               <?php if ($result['status'] == 'active') { ?>
               <a class="" href="<?=$this->url('admin_notapproved_comment', ['id' => $result['id'], 'fromPage' => $actualPageId] ) ?>">
-                <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Approuver">
-                   <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                <button type="button" class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Approuver">
+                   <i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
                 </button>
              </a> <?php } ?>
               <!-- BOUTON : Delete -->
