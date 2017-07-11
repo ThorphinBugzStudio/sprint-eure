48bffb8eb0e4d68165f68448287c0a98e6b723d0
Vox <test@test.fr>
2017-07-11 22:33:18 +0200

    CSS Front Connexion

--- app/Views/users/login.php
+++ app/Views/users/login.php
@@ -1,28 +1,35 @@
-<?php $this->layout('layout', ['title' => 'login']) ?>
+<?php $this->layout('layout', ['title' => 'Connexion']) ?>
 
 <?php $this->start('main_content') ?>
-<div class="flash-message">
+    <div class="flash-message">
+      <?php if(!empty($w_flash_message->message)) { ?>
+        <span class="span-flash"><?= $w_flash_message->message ?></span>
+    <!-- echo $w_flash_message->level; interprete l'argument success ou warning.... pr bootstrap(couleurs)-->
+        <?php } ?>
+    </div>
 
-  <?php if(!empty($w_flash_message->message)) { ?>
+    <div class="container connexion">
+      <form class="row my-3" action="<?php $this->url('login_action') ?>" method="POST">
 
-    <span class="span-flash"><?= $w_flash_message->message ?></span>
-<!-- echo $w_flash_message->level; interprete l'argument success ou warning.... pr bootstrap(couleurs)-->
-    <?php } ?>
+        <!-- Catégorie : Pseudo -->
+        <label for="pseudo-mail">Pseudo / E-mail :</label>
+        <input class="input-form" type="text" name="pseudo-mail" value="">
+        <?php if(!empty($error['pseudo-mail'])){ echo '<p id="error-firstname" class="error">' . $error['pseudo-mail'] . '</p>'; } ?>
 
-    <form class="" action="<?php $this->url('login_action') ?>" method="POST">
+        <br>
 
-      <label for="pseudo-mail">Entrez votre pseudo ou votre Email<span><?php if(!empty($error['pseudo-mail'])){ echo $error['pseudo-mail']; } ?></span></label>
-      <input type="text" name="pseudo-mail" value="">
+        <!-- Catégorie : Mot de passe -->
+        <label for="password" class="mt-2">Mot de passe :</label>
+        <input class="input-form" type="password" name="password" value="">
+        <?php if(!empty($error['password'])){ echo '<p id="error-firstname" class="error">' . $error['password'] . '</p>'; } ?>
 
-      <label for="password">Entrez votre mot de passe<span><?php if(!empty($error['password'])){ echo $error['password']; } ?></span></label>
-      <input type="text" name="password" value="">
-
-      <input type="submit" name="submit" value="Se connecter">
-
-      <a href="<?php echo $this->url('password_lost'); ?>">Mot de passe oublié?</a>
-
-    </form>
-
+        <!-- Catégorie : Boutons d'action -->
+        <div class="row mx-auto mt-3 justify-content-center">
+            <input type="submit" name="submit" value="Se connecter" class="btn_ok m-2">
+            <a href="<?php echo $this->url('password_lost'); ?>" class="btn_cancel m-2">Mot de passe oublié?</a>
+        </div>
+      </form>
+    </div>
 
 
 <?php $this->stop('main_content') ?>
