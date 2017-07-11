9293bbf9a43e072b16ddb46c90e869cb14eedd08
Maximus <maximefauvelliere@gmail.com>
2017-07-11 17:03:25 +0200

    fin de journéee 11/07

--- app/Views/users/user-profile.php
+++ app/Views/users/user-profile.php
@@ -2,11 +2,25 @@
 
 <?php $this->start('main_content');
 
-debug($user);
-$user_id = $user['id']; ?>
+debug($w_user);
+debug($user_adress);
+debug($user_avatar);
+$user_id = $w_user['id']; ?>
 
-<p>Pseudo<?= ' '.$user['username'].' ' ?><a href="<?php $this->url('user_profile_modify',['id' => $user_id]); ?>">Edit</a></p>
+<p>Pseudo   <?= $w_user['username'] ?></p>
+<p>Nom   <?= $w_user['lastName'] ?></p>
+<p>prénom   <?= $w_user['firstName'] ?></p>
+<p>Email   <?= $w_user['email'] ?></p>
 
+  <p>Adresse   <?= $user_adress['adress1'] ?></p>
+  <p>Code postal   <?= $user_adress['postal_code'] ?></p>
+  <p>Ville   <?= $user_adress['town'] ?></p>
+  <p>Pays   <?= $user_adress['country'] ?></p>
+  <p>Avatar   <img src="<?= $this->assetUrl('img/avatars/'.$user_avatar['img_name']); ?>" alt=""><?= $user_avatar['img_name'].' ' ?></p>
+
+
+
+<a href="<?= $this->url('user_profile_modify',['id' => $user_id]); ?>">Editer votre profil</a>
 
 
 
