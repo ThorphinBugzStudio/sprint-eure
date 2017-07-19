527f58d9d25458846099c713f54226a7a31a200d
quidelantoine <quidelantoine@gmail.com>
2017-07-19 15:36:30 +0200

    password modify

--- app/Views/users/password-modify.php
+++ app/Views/users/password-modify.php
@@ -5,13 +5,13 @@
 <form class="" action="<?php $this->url('password_modify_action') ?>" method="POST">
   <div class="row mx-auto mt-4" style="max-width: 300px;">
     <label for="newpassword">Votre nouveau mot de passe :</label> <br>
-    <input class="input-form" type="text" name="newpassword" value="<?php if(!empty($_POST['newpassword'])){ echo $_POST['newpassword']; } ?>">
+    <input class="input-form" type="password" name="newpassword" value="<?php if(!empty($_POST['newpassword'])){ echo $_POST['newpassword']; } ?>">
     <span class="error mx-auto mb-2"><?php if(!empty($error['newpassword'])){ echo $error['newpassword'];} ?></span>
   </div>
 
   <div class="row mx-auto" style="max-width: 300px;">
     <label for="newpassword-confirm">Confirmation de votre mot de passe :</label> <br>
-    <input class="input-form" type="text" name="newpassword-confirm" value="<?php if(!empty($_POST['newpassword-confirm'])){ echo $_POST['newpassword-confirm']; } ?>">
+    <input class="input-form" type="password" name="newpassword-confirm" value="<?php if(!empty($_POST['newpassword-confirm'])){ echo $_POST['newpassword-confirm']; } ?>">
   </div>
 
   <div class="row mt-3 mb-2">
