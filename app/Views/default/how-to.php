be7457a52d1b62142cb059bc65df136ed111f53a
Maximus <maximefauvelliere@gmail.com>
2017-07-19 10:26:27 +0200

    pas grd chose

--- app/Views/default/how-to.php
+++ app/Views/default/how-to.php
@@ -17,7 +17,7 @@
 
     <p class="text-justify mt-3">
       Mais au fait, comment ça marche une imprimante 3D ?
-      Depuis les logiciels de conception assistés par Ordinateur (CAO) jusqu’à la
+      Depuis les logiciels de conception assistée par Ordinateur (CAO) jusqu’à la
       création de l’objet, vous saurez tout du fonctionnement de l’imprimante 3D
       grâce à notre rubrique. Mais si le fonctionnement est une chose,
       le potentiel en est une autre et c’est bien pour cela que ces machines passionnent
@@ -35,7 +35,7 @@
       Peu encombrante et peu coûteuse (vis-à-vis des moyens de production « traditionnels »),
       elle est potentiellement le moyen pour n’importe quel quidam de concevoir et
       produire des objets usuels et disponibles en un rien de temps.
-      Les débouchées sont multiples. Santé, développement économique ou tout simplement
+      Les débouchés sont multiples. Santé, développement économique ou tout simplement
       la création de la petite vis de substitution pour l’armoire de la salle à manger
       ou un double de ses clés.
     </p>
@@ -61,7 +61,7 @@
       Il existe un nombre important de logiciels sur le marché qui permettent de créer
       ses modèles 3D (certains gratuits et/ou Open Source d’autres sont propriétaires).
       Les plans sont ensuite transmis à l’imprimante.
-      Et la magie logicielle ne ça ne s’arrête pas là. L’imprimante est obligé de traiter
+      Et la magie logicielle ne s’arrête pas là. L’imprimante est obligée de traiter
       cette modélisation 3D conformément à la logique de Fabrication Additive :
       c’est-à-dire découper la modélisation 3D en couche 2D.
       Un nouveau logiciel prend alors le pas pour créer ces découpes 2D et envoyer les instructions à l’imprimante.
@@ -93,11 +93,11 @@
       L’imprimante 3D reçoit donc une série d’instructions mais plusieurs technologies
       existent pour passer de couches 2D à un objet 3D.
       Deux grands types de procédés sont en fait utilisés pour former les objets.
-      Soit la matière travaillée peut entrer en fusion et en quel cas, elle peut être
+      Soit la matière travaillée peut entrer en fusion et au quel cas, elle peut être
       fondue peu à peu pour obtenir la forme souhaitée. Soit la matière peut être solidifiée
       sous l’action de la chaleur ou de la lumière (laser). Mais systématiquement,
       l’imprimante 3D « pense » couche par couche pour obtenir la forme 3D.
-      Chacun des méthodes implique des qualités propres qui jouent sur la durée nécessaire
+      Chacune des méthodes impliquent des qualités propres qui jouent sur la durée nécessaire
       à l’obtention de l’objet mais aussi la taille limite des objets .
     </p>
 
